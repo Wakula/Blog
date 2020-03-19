316adb56282a2803aae781d424011f8d57047714
Cyryl Vakulenko <wither04@gmail.com>
2020-03-19 21:08:42 +0200

    add view for start page

--- src/blog_app/views.py
+++ src/blog_app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import View
 
-# Create your views here.
+
+class IndexView(View):
+    def get(self, request):
+        if request.user.is_superuser:
+            return redirect('/admin/')
+        if request.user.is_authenticated:
+            return render('index.html')
+        return redirect('login_url')
