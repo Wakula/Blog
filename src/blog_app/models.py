d7038fe20564841ecfb237a03dee6a8005f3c53c
Cyryl Vakulenko <wither04@gmail.com>
2020-03-20 01:35:36 +0200

    add Post and Blog models

--- src/blog_app/models.py
+++ src/blog_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Blog(models.Model):
+    name = models.CharField(max_length=80)
+    description = models.TextField()
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    subscribers = models.ManyToManyField(get_user_model(), blank=True, related_name='subscribed_blogs')
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=80)
+    content = models.TextField()
+    date_pub = models.DateTimeField(auto_now_add=True)
+    users_that_have_read = models.ManyToManyField(get_user_model(), blank=True, related_name='read_posts')
+
+    def __str__(self):
+        return f'{self.title}'
