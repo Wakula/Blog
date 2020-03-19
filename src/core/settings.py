496feeeeb82f74531afa9c6f3658da556e1cc65e
Cyryl Vakulenko <wither04@gmail.com>
2020-03-19 21:46:51 +0200

    setup blog_app

--- src/core/settings.py
+++ src/core/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL = 'home'
