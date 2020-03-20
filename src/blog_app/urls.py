d137fc5e7c666827c39fbcf702fefae7bd7710ad
Cyryl Vakulenko <wither04@gmail.com>
2020-03-20 15:21:27 +0200

    add post url and rename SinglePostView

--- src/blog_app/urls.py
+++ src/blog_app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login_url'),
     path('logout/',LogoutView.as_view(next_page='home_url'), name='logout_url'),
     path('blog/', views.UserBlogView.as_view(), name='blog_url'),
+    path('post/', views.PostCreationView.as_view(), name='post_url'),
 ]
