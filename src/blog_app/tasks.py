ef5eb97354ccf87a2de79b16ba393977bccb348d
Cyryl Vakulenko <wither04@gmail.com>
2020-03-21 00:31:09 +0200

    setup celery and email_task

---
+++ src/blog_app/tasks.py
@@ -0,0 +1,16 @@
+from celery import shared_task
+from django.core.mail import send_mail
+from django.conf import settings
+
+from time import sleep
+
+
+@shared_task
+def email_task(title, blog_name, post_id, email):
+    send_mail(
+        f'New post in {title}', 
+        None,
+        settings.EMAIL_HOST_USER,
+        [email],
+        html_message=f'<a href="http://localhost:8000/post/{post_id}/">{email}</a>'
+    )
