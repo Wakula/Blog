227dc288273e433522e173e39efb7cc2516a151f
Cyryl Vakulenko <wither04@gmail.com>
2020-03-20 14:36:05 +0200

    add url for blog

--- src/blog_app/urls.py
+++ src/blog_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='home_url'),
     path('login/', LoginView.as_view(), name='login_url'),
     path('logout/',LogoutView.as_view(next_page='home_url'), name='logout_url'),
+    path('blog/', views.UserBlogView.as_view(), name='blog_url'),
 ]
